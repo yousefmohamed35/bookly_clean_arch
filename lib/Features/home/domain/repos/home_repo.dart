7871fddceb496a134b78c385e2aa725d6ab2d90e
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-10-24 19:41:47 +0300

    create home repo

---
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -0,0 +1,6 @@
+import 'package:booklycleanarch/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
