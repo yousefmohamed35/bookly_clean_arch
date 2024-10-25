c6017b731b957920ae3f9a9eccc11d0d296b2529
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-10-25 15:31:34 +0300

    fetch newest book use case

---
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:booklycleanarch/Features/home/domain/entities/book_entity.dart';
+import 'package:booklycleanarch/Features/home/domain/repos/home_repo.dart';
+import 'package:booklycleanarch/core/errors/failure.dart';
+import 'package:booklycleanarch/core/use_case/no_param_use_case.dart';
+
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase({required this.homeRepo});
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() {
+    return homeRepo.fetchNewestBooks();
+  }
+}
