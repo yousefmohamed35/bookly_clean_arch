805ee8b836615cad06c7f0db4ba0e875429ae8f7
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-10-27 23:14:03 +0300

    generate book entity adapter

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,12 +1,22 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
 class BookEntity {
-  final String? image;
-  final String title;
-  final String? authorName;
-  final num? price;
-  final num? rating;
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
+  final String? image;
+  @HiveField(2)
+  final String title;
+  @HiveField(3)
+  final String? authorName;
+  @HiveField(4)
+  final num? price;
+  @HiveField(5)
+  final num? rating;
 
-  BookEntity({required this.bookId, 
+  BookEntity(
+      {required this.bookId,
       required this.image,
       required this.title,
       required this.authorName,
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  
   runApp(const Bookly());
 }
 
