83c9402063eb98e3bdaa97c9c05fbad1c0b99ad7
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-10-28 14:52:12 +0300

    create newest books cubit

--- lib/Features/home/presentation/manager/newest_books/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/newest_books/newest_books_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:booklycleanarch/Features/home/domain/entities/book_entity.dart';
+import 'package:booklycleanarch/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
-
-  
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold((failure) {
+      emit(
+        NewestBooksFailure(errorMessage: failure.message),
+      );
+    }, (books) {
+      emit(
+        NewestBooksSuccess(books: books),
+      );
+    });
+  }
 }
